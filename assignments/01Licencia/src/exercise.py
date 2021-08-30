fe46c808a3c647af9d95bd9b57d8018290c7b4c0
Alan De Alba <A01177873@itesm.mx>
2021-08-29 19:31:07 -0500

    Tarea Licencia A01177873

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,5 +1,16 @@
-
 def main():
+    edad = int(input("Ingresa tu edad: "))
+    if edad >= 18:
+        id = input("¿Tienes identificación oficial? (s/n): ")
+        if id == "s":
+            print("Trámite de licencia concedido")
+        elif id == "n":
+            print("No cumples requisitos")
+        elif id != "s" or "s":
+            print("Respuesta incorrecta")
+    else:
+        print("No cumples requisitos")
+    
     pass
 
 
