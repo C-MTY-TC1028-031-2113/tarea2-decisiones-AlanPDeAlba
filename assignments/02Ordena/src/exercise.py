c6de707793b1a82731018f9901cfce5de49493bd
Alan De Alba <A01177873@itesm.mx>
2021-08-29 19:39:43 -0500

    Tarea Ordena A01177873

--- assignments/02Ordena/src/exercise.py
+++ assignments/02Ordena/src/exercise.py
@@ -3,8 +3,30 @@ def main():
     x = int(input("Ingresa el primer número: "))
     y = int(input("Ingresa el segundo número: "))
     z = int(input("Ingresa el tercer número: "))
-    
-
+    if x > y > z:
+        print(x)
+        print(y)
+        print(z)
+    elif x > z > y:
+        print(x)
+        print(z)
+        print(y)
+    elif y > x > z:
+        print(y)
+        print(x)
+        print(z)
+    elif y > z > x:
+        print(y)
+        print(z)
+        print(x)
+    elif z > x > y:
+        print(z)
+        print(x)
+        print(y)
+    elif z > y > x:
+        print(z)
+        print(y)
+        print(x)
 
 if __name__=='__main__':
     main()
