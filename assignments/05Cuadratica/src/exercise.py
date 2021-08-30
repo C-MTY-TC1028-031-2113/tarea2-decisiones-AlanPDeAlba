b82d35d7f6cacf183555f1cbf0f5b90c0cebe806
Alan De Alba <A01177873@itesm.mx>
2021-08-29 20:30:24 -0500

    Tarea Cuadratica A01177873

--- assignments/05Cuadratica/src/exercise.py
+++ assignments/05Cuadratica/src/exercise.py
@@ -5,6 +5,18 @@ def main():
     a=int(input("Da el valor de a: "))
     b=int(input("Da el valor de b: "))
     c=int(input("Da el valor de c: "))
+    if a == 0 and b == 0:
+        print("No tiene solucion")
+    elif a == 0 and b != 0:
+        print((-1*c)/b)
+    elif a != 0 and b != 0:
+        if ((b**2) - 4*a*c) < 0:
+            print("Raices complejas")
+        elif ((b**2) - 4*a*c) == 0:
+            print((-1*b)/(2*a))
+        elif ((b**2) - 4*a*c) > 0:
+            print(((-1*b) + math.sqrt(b**2 - (4*a*c)))/(2*a))
+            print(((-1*b) - math.sqrt(b**2 - (4*a*c)))/(2*a))
     pass
 
 if __name__ == '__main__':
